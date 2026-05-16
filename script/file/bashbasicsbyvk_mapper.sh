4a9193eea05adace20dbdd8fd66dfdf45e340094
vk datta <134008395+vkdatta@users.noreply.github.com>
2026-05-16 09:05:31 +0530

    Update bashbasicsbyvk_mapper.sh

--- script/file/bashbasicsbyvk_mapper.sh
+++ script/file/bashbasicsbyvk_mapper.sh
@@ -1,33 +1,36 @@
 map_directory() {
-
     local target_dir="${1:-$path}"
 
     generate_map() {
+        (
+            cd "$target_dir" || exit
 
-        find "$target_dir" -print | \
-        sed -e 's;[^/]*/;│   ;g' \
-            -e 's;│   \([^│]\);├── \1;'
+            find . | sed \
+                -e '1d' \
+                -e 's|^\./||' \
+                -e 's|[^/]*/|│   |g' \
+                -e 's|│   \([^│]\)|├── \1|'
+        )
     }
 
     copy_to_clipboard() {
-
         local content="$1"
 
         printf "%s" "$content" | bashbasicsbyvk_copy
     }
 
     save_to_file() {
-
         local content="$1"
 
         read -p "Enter file name: " filename
 
-        printf "%s" "$content" > "$filename"
+        printf "%s" "$content" > "$target_dir/$filename"
 
-        echo "output saved as $filename"
+        echo "output saved as $target_dir/$filename"
     }
 
     local map_output
+    local choice
 
     map_output=$(generate_map)
 
@@ -39,7 +42,6 @@ map_directory() {
     read -p "Choose option [1-2]: " choice
 
     case "$choice" in
-
         1)
             copy_to_clipboard "$map_output"
             echo "output copied to clipboard"
