63123349914d60ee8a64871263c74c8bc2b85df1
vkdatta <vkyd2016@gmail.com>
2026-06-24 01:36:44 +0530

    Codebase Updated

--- setup.py
+++ setup.py
@@ -2,9 +2,23 @@ import os
 import glob
 from setuptools import setup
 
-all_paths = glob.glob('script/**/*', recursive=True)
-script_files = [f for f in all_paths if os.path.isfile(f)]
+all_paths = glob.glob("script/**/*", recursive=True)
+
+script_files = [
+    f for f in all_paths
+    if os.path.isfile(f)
+    and not f.endswith((".xlsx", ".txt"))
+]
+
+resource_files = [
+    f for f in all_paths
+    if os.path.isfile(f)
+    and f.endswith((".xlsx", ".txt"))
+]
 
 setup(
-    scripts=script_files
+    scripts=script_files,
+    data_files=[
+        ("bashbasicsbyvk", resource_files),
+    ],
 )
